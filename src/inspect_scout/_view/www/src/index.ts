ca5af815d3949f847bacdeaa4a53db5c096b21d1
Sami Jawhar <sami@metr.org>
2025-12-23 20:20:17 +0000

    2025-12-23 release

--- src/inspect_scout/_view/www/src/index.ts
+++ src/inspect_scout/_view/www/src/index.ts
@@ -2,10 +2,11 @@
 export { App } from "./App";
 
 // Client APIs
+export { apiScoutServerV1 } from "./api/api-scout-server-v1.ts";
 export { apiScoutServer } from "./api/api-scout-server.ts";
 
 // Client API - Types
 export type { ScanApi } from "./api/api.ts";
 
 // State Store
-export { ApiProvider, StoreProvider, createStore } from "./state/store";
+export { ApiProvider, createStore, StoreProvider } from "./state/store";
